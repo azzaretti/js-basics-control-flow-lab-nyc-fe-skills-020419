c401414d1859f892aa95d387628ae3a1591668a5
Greg Azzaretti <azzaretti.greg@gmail.com>
2019-03-11 23:54:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function scuberGreetingForFeet (rideLength){
   if (rideLength <= 400){
   return 'This one is on me!'
 } else if (rideLength > 2000) {
-  return ''
+  return 'I will gladly take your thirty bucks'
 }
 }
